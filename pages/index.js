c55fba14f22866555b201672a65c8f95c9e160d9
User <user@iMac-cua-User.local>
2025-07-20 07:43:50 +0700

    JS Simple NExt

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+import Head from 'next/head'
+import Image from 'next/image'
+import {Component} from "react"
+import styles from '../styles/Home.module.css'
+
+export default class Home extends Component {
+	constructor(props){
+    	super(props)
+    	this.state = {
+
+    	}
+	}
+
+	componentDidMount(){
+
+	}
+
+	render(){
+		return (
+			<div> BRUM </div>
+		)
+	}
+}
